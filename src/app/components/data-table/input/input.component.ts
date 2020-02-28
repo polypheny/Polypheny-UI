df3060ef85b31410ec246213325361ba45dd2ce9
Nils Hansen <n.hansen@stud.unibas.ch>
2020-02-28 21:11:04 +0100

    Integrate flatpickr plugin Providing input elements for date/time types (polypheny-db/Polypheny-DB#63)

--- src/app/components/data-table/input/input.component.ts
+++ src/app/components/data-table/input/input.component.ts
@@ -1,34 +1,56 @@
-import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import {DbColumn} from '../models/result-set.model';
 import {DbmsTypesService} from '../../../services/dbms-types.service';
 import * as $ from 'jquery';
+import flatpickr from 'flatpickr';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent implements OnInit, OnChanges {
+export class InputComponent implements OnInit, OnChanges, AfterViewInit {
 
   @Input() header: DbColumn;
   @Input() value;
   @Output() valueChange = new EventEmitter();
   @Output() enter = new EventEmitter();
   @ViewChild('inputElement', {static: false}) inputElement: ElementRef;
+  @ViewChild('flatpickr', {static: false}) flatpickrElement: ElementRef;
+  flatpickrObj;
 
   constructor(
     public _types: DbmsTypesService
-  ) { }
+  ) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initFlatpickr();
+  }
 
   ngOnChanges(changes: SimpleChanges) {
-    if( this.inputElement !== undefined && changes.value && changes.value.currentValue === null ){
+    if (this.inputElement !== undefined && changes.value && changes.value.currentValue === null) {
       $(this.inputElement.nativeElement).removeClass('is-valid').removeClass('is-invalid');
+    } else if (this._types.isDateTime(this.header.dataType) && (changes.value.currentValue == null || changes.value.currentValue === '') && this.flatpickrObj) {
+      this.flatpickrObj.setDate(null);
     }
   }
 
-  triggerNull ( value ) {
+  triggerNull(value) {
     if( value !== null) return null;
     else{
       if( this._types.isNumeric( this.header.dataType )){
@@ -53,14 +75,53 @@ export class InputComponent implements OnInit, OnChanges {
     this.validate( newVal, inputElement );
   }
 
-  validate ( val, inputElement ) {
-    if( isNaN( val ) ){
+  validate(val, inputElement) {
+    if (isNaN(val)) {
       $(inputElement).addClass('is-invalid').removeClass('is-valid');
-    } else if ( !isNaN( val ) && val !== null ) {
+    } else if (!isNaN(val) && val !== null) {
       $(inputElement).addClass('is-valid').removeClass('is-invalid');
     } else {
       $(inputElement).removeClass('is-valid').removeClass('is-invalid');
     }
+  }
+
+  initFlatpickr() {
+    // https://flatpickr.js.org/options/
+    const self = this;
+
+    function onChange(selectedDates, dateStr, instance) {
+      self.onValueChange(dateStr);
+    }
+
+    switch (this.header.dataType.toLowerCase()) {
+      case 'date':
+        this.flatpickrObj = flatpickr(this.flatpickrElement.nativeElement, {
+          dateFormat: 'Y-m-d',
+          onChange: onChange
+        });
+        break;
+      case 'time':
+        this.flatpickrObj = flatpickr(this.flatpickrElement.nativeElement, {
+          enableTime: true,
+          noCalendar: true,
+          dateFormat: 'H:i:S',
+          time_24hr: true,
+          onChange: onChange
+        });
+        break;
+      case 'timestamp':
+        this.flatpickrObj = flatpickr(this.flatpickrElement.nativeElement, {
+          enableTime: true,
+          dateFormat: 'Y-m-d H:i:S',
+          time_24hr: true,
+          onChange: onChange
+        });
+        break;
+    }
+  }
+
+  clearFlatpickr() {
+    this.valueChange.emit(null);
   }
 
 }
