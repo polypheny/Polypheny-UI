ae7265ec605ff745e52934b103ee7bbd4691e4bb
alyaa999 <alyaamamoon999@gmail.com>
2024-03-07 20:41:17 +0200

    Enlarge images and videos on click - add light box to data-card - add light box to data-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,85 +1,97 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
-import {PerfectScrollbarConfigInterface, PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {
+  PerfectScrollbarConfigInterface,
+  PerfectScrollbarModule,
+} from "ngx-perfect-scrollbar";
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LightboxModule } from "ngx-lightbox";
 
 // coreui / bootstrap imports
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import { TooltipModule } from "ngx-bootstrap/tooltip";
 // tree-module
-import {TreeModule} from 'angular-tree-component';
-import {AppComponent} from './app.component';
+import { TreeModule } from "angular-tree-component";
+import { AppComponent } from "./app.component";
 
 // Import routing module
-import {AppRoutingModule} from './app.routing';
+import { AppRoutingModule } from "./app.routing";
 
 // Import 3rd party components
-import {TypeaheadModule} from 'ngx-bootstrap/typeahead';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
-import {TabsModule} from 'ngx-bootstrap/tabs';
-import {ChartsModule} from 'ng2-charts';
-import {HttpClientModule} from '@angular/common/http';
-import {ComponentsModule} from './components/components.module';
-import {AppAsideModule, AppFooterModule, AppHeaderModule, AppSidebarModule} from '@coreui/angular';
-import {DefaultLayoutComponent} from './containers/default-layout';
-import {P404Component} from './views/error/404.component';
-import {P500Component} from './views/error/500.component';
-import {LoginComponent} from './views/login/login.component';
-import {ViewsModule} from './views/views.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {PopoverModule} from 'ngx-bootstrap/popover';
-import {ModalModule} from 'ngx-bootstrap/modal';
-import {NgxJsonViewerModule} from 'ngx-json-viewer';
-import {NotebooksModule} from './plugins/notebooks/notebooks.module';
+import { TypeaheadModule } from "ngx-bootstrap/typeahead";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { ChartsModule } from "ng2-charts";
+import { HttpClientModule } from "@angular/common/http";
+import { ComponentsModule } from "./components/components.module";
+import {
+  AppAsideModule,
+  AppFooterModule,
+  AppHeaderModule,
+  AppSidebarModule,
+} from "@coreui/angular";
+import { DefaultLayoutComponent } from "./containers/default-layout";
+import { P404Component } from "./views/error/404.component";
+import { P500Component } from "./views/error/500.component";
+import { LoginComponent } from "./views/login/login.component";
+import { ViewsModule } from "./views/views.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PopoverModule } from "ngx-bootstrap/popover";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { NgxJsonViewerModule } from "ngx-json-viewer";
+import { NotebooksModule } from "./plugins/notebooks/notebooks.module";
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-    suppressScrollX: true
+  suppressScrollX: true,
 };
 
 @NgModule({
-    imports: [
-        ComponentsModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        PerfectScrollbarModule,
-        BsDropdownModule.forRoot(),
-        TabsModule.forRoot(),
-        ChartsModule,
-        // plugins
-        NotebooksModule,
-        // coreui / bootstrap
-        TooltipModule.forRoot(),
-        AppHeaderModule,
-        AppAsideModule,
-        AppSidebarModule,
-        AppFooterModule,
-        // forms
-        FormsModule, ReactiveFormsModule,
-        TreeModule.forRoot(),
-        BsDropdownModule,
-        TypeaheadModule.forRoot(),
-        HttpClientModule,
-        ViewsModule,
-        PopoverModule.forRoot(),
-        ModalModule.forRoot(),
-        NgxJsonViewerModule
-    ],
-    declarations: [
-        AppComponent,
-        DefaultLayoutComponent,
-        P404Component,
-        P500Component,
-        LoginComponent,
-    ],
-    providers: [{
-        provide: LocationStrategy,
-        useClass: HashLocationStrategy
-    }],
-    bootstrap: [AppComponent],
-    exports: []
+  imports: [
+    ComponentsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    PerfectScrollbarModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ChartsModule,
+    // plugins
+    NotebooksModule,
+    LightboxModule,
+    // coreui / bootstrap
+    TooltipModule.forRoot(),
+    AppHeaderModule,
+    AppAsideModule,
+    AppSidebarModule,
+    AppFooterModule,
+    // forms
+    FormsModule,
+    ReactiveFormsModule,
+    TreeModule.forRoot(),
+    BsDropdownModule,
+    TypeaheadModule.forRoot(),
+    HttpClientModule,
+    ViewsModule,
+    PopoverModule.forRoot(),
+    ModalModule.forRoot(),
+    NgxJsonViewerModule,
+  ],
+  declarations: [
+    AppComponent,
+    DefaultLayoutComponent,
+    P404Component,
+    P500Component,
+    LoginComponent,
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
+  bootstrap: [AppComponent],
+  exports: [],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/components/data-view/data-card/data-card.component.ts
+++ src/app/components/data-view/data-card/data-card.component.ts
@@ -1,49 +1,107 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {CrudService} from '../../../services/crud.service';
-import {ToastService} from '../../toast/toast.service';
-import {DbmsTypesService} from '../../../services/dbms-types.service';
-import {BsModalService} from 'ngx-bootstrap/modal';
-import {DataViewComponent} from '../data-view.component';
-import {WebuiSettingsService} from '../../../services/webui-settings.service';
-import {LeftSidebarService} from '../../left-sidebar/left-sidebar.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CrudService } from "../../../services/crud.service";
+import { ToastService } from "../../toast/toast.service";
+import { DbmsTypesService } from "../../../services/dbms-types.service";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { DataViewComponent } from "../data-view.component";
+import { WebuiSettingsService } from "../../../services/webui-settings.service";
+import { LeftSidebarService } from "../../left-sidebar/left-sidebar.service";
+import { Lightbox , LightboxConfig } from "ngx-lightbox";
 
 @Component({
-    selector: 'app-data-card',
-    templateUrl: './data-card.component.html',
-    styleUrls: ['./data-card.component.scss']
+  selector: "app-data-card",
+  templateUrl: "./data-card.component.html",
+  styleUrls: ["./data-card.component.scss"],
 })
 export class DataCardComponent extends DataViewComponent implements OnInit {
+  showInsertCard = false;
+  jsonValid = false;
+   album = [];
 
-    showInsertCard = false;
-    jsonValid = false;
 
-    constructor(
-        public _crud: CrudService,
-        public _toast: ToastService,
-        public _route: ActivatedRoute,
-        public _router: Router,
-        public _types: DbmsTypesService,
-        public _settings: WebuiSettingsService,
-        public _sidebar: LeftSidebarService,
-        public modalService: BsModalService
-    ) {
-        super(_crud, _toast, _route, _router, _types, _settings, _sidebar, modalService);
+  
+
+  constructor(
+    public _crud: CrudService,
+    public _toast: ToastService,
+    public _route: ActivatedRoute,
+    public _router: Router,
+    public _types: DbmsTypesService,
+    public _settings: WebuiSettingsService,
+    public _sidebar: LeftSidebarService,
+    public modalService: BsModalService,
+    public  lightbox: Lightbox, public _lightboxConfig: LightboxConfig) {
+    super(
+      _crud,
+      _toast,
+      _route,
+      _router,
+      _types,
+      _settings,
+      _sidebar,
+      modalService
+    );
+    // _lightboxConfig.fadeDuration = .3;
+
+
+
+  }
+
+  ngOnInit(): void {
+    if (this.config && this.config.create) {
+      this.buildInsertObject();
     }
+    this.setPagination();
+    this.loadAlbum();
 
-    ngOnInit(): void {
-        if (this.config && this.config.create) {
-            this.buildInsertObject();
+  
+  }
+
+loadAlbum(){     
+  for (var i = 0; i < this.resultSet.data.length; i++) {
+      for (var j = 0; j < this.resultSet.header.length; j++) {
+        if (
+          this.resultSet.header[j].dataType === "IMAGE" ||
+          this.resultSet.header[j].dataType === "VIDEO"
+        ) {
+          var info = "";
+          // Concatenate information from all rows as caption
+          for (var dataIndex = 0; dataIndex< this.resultSet.header.length; dataIndex++) {
+            // Skip the row being used as the image source
+            if (dataIndex !== j) {
+          
+              info +=  this.resultSet.header[dataIndex].name + " :  " +this.resultSet.data[i][dataIndex] + " , "; 
+            }
+          }
+
+          const image = {
+            src: this.getFileLink(this.resultSet.data[i][j]),
+            caption: info, 
+            thumb: ""
+          };
+          this.album.push(image);
         }
-        this.setPagination();
+      }
     }
+    console.log(this.album);
+  
+}
 
-    setJsonValid($event: any) {
-        this.jsonValid = $event;
-    }
+  setJsonValid($event: any) {
+    this.jsonValid = $event;
 
-    showInsert() {
-        this.editing = null;
-        this.showInsertCard = true;
-    }
+  }
+
+  showInsert() {
+    this.editing = null;
+    this.showInsertCard = true;
+  }
+
+  openLightbox(i: number): void {
+    this.lightbox.open(this.album , i);
+  }
+
+
+  
 }
