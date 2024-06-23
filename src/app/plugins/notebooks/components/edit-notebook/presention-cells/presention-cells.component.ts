43f679b63195781b492214cb0e9c5fa05d92af81
Eslam Ashraf <eslamashraf.12342@gmail.com>
2024-06-23 15:53:07 +0300

    add output of code && add raw cell for presentaion

--- src/app/plugins/notebooks/components/edit-notebook/presention-cells/presention-cells.component.ts
+++ src/app/plugins/notebooks/components/edit-notebook/presention-cells/presention-cells.component.ts
@@ -7,6 +7,9 @@ import {KatexOptions, MarkdownService} from 'ngx-markdown';
 import { Slides } from '../../../models/peresentions';
 import { Router } from '@angular/router';
 import { HighlightService } from '../../../services/highlight.service'
+import { CellErrorOutput, CellStreamOutput, NotebookCell} from '../../../models/notebook.model';
+import {DomSanitizer} from '@angular/platform-browser';
+import {default as AnsiUp} from 'ansi_up';
 
 @Component({
   selector: 'app-presention-cells',
@@ -23,17 +26,24 @@ export class PresentionCellsComponent implements OnInit {
 
 
   private highlighted: boolean = false
+  private ansi_up = new AnsiUp();
 
 constructor(private _markdown: MarkdownService,
   public router:Router,
-  private highlightService: HighlightService) {
+  private highlightService: HighlightService,
+  private _sanitizer: DomSanitizer,) {
 }
 
   ngOnInit(): void {
+    
+    console.log(   this.nb.cells)
     this.slides=[];
     this.nb.cells.forEach(item=>{
+     
+      /* store data for presention */
       let type=this.nb.getCellPresent(item);
       let codeType=this.nb.getCellType(item);
+      let isTrusted=this.nb.trustedCellIds.has(item.id)
       if(type!="skip")
       {
         let language=""
@@ -43,42 +53,37 @@ constructor(private _markdown: MarkdownService,
         }
         else if (codeType=="code")
         {
-          language="py"
+          language="py";
+          const output = item?.outputs.find(o => o.output_type === 'error');
+          if (output) {
+              var errorHtml= this.renderError(<CellErrorOutput>output);
+          }
+          const stream = item?.outputs.find(o => o.output_type === 'stream');
+          if (stream) {
+              var streamHtml =this.renderStream(<CellStreamOutput>stream);
+          }
         }
         if(type=="slide" || this.slides.length==0)
         {
-          if(language=="")
-          {
-            this.slides.push({source:item.source,children:[{source:item.source,children:[],fragments:[],type:item.cell_type}],fragments:[],type:item.cell_type});
-          }
-          else{
-            this.slides.push({source:item.source,children:[{source:item.source,children:[],fragments:[],type:item.cell_type,language:language}],fragments:[],type:item.cell_type,language:language});
+            this.slides.push({source:item.source,children:[{source:item.source,children:[],fragments:[],type:item.cell_type,language:language,
+              isTrusted:isTrusted,outputs:item?.outputs,errorHtml:errorHtml,streamHtml:streamHtml}],fragments:[],type:item.cell_type,language:language,isTrusted:isTrusted,outputs:item?.outputs,errorHtml:errorHtml,streamHtml:streamHtml});
 
-          }
         }
         else if (type=="fragment")
         {
           let len=this.slides[this.slides.length-1].children.length;
-          if(language=="")
-          {
-          this.slides[this.slides.length-1].children[len-1].fragments.push({text:item.source,type:item.cell_type});
-          }
-          else{
+       
             this.slides[this.slides.length-1].children[len-1].fragments.push({text:item.source,type:item.cell_type,language:language});
 
-          }
         }
         else {
-          if(language=="")
-          {
-          this.slides[this.slides.length-1].children.push({source:item.source,children:[],fragments:[],type:item.cell_type});
-          }
-          else{
-            this.slides[this.slides.length-1].children.push({source:item.source,children:[],fragments:[],type:item.cell_type,language:language});
-          }
+        
+            this.slides[this.slides.length-1].children.push({source:item.source,children:[],fragments:[],type:item.cell_type,language:language,isTrusted:isTrusted,outputs:item?.outputs,errorHtml:errorHtml,streamHtml:streamHtml});
         }
       }
     })
+    console.log(   this.slides)
+
   }
   
   ngAfterViewInit(){  
@@ -107,6 +112,19 @@ constructor(private _markdown: MarkdownService,
       
     }
   }
- 
+  renderError(output: CellErrorOutput) {
+    if (output) {
+      return this._sanitizer.bypassSecurityTrustHtml(this.ansi_up.ansi_to_html(output.traceback.join('\n')));
+    }
+    return null;
+  }
+
+  renderStream(output: CellStreamOutput) {
+      if (output) {
+          const text = Array.isArray(output.text) ? output.text.join('\n') : output.text;
+          return this._sanitizer.bypassSecurityTrustHtml(this.ansi_up.ansi_to_html(text));
+      }
+      return null;
+  }
 
 }
--- src/app/plugins/notebooks/models/peresentions.ts
+++ src/app/plugins/notebooks/models/peresentions.ts
@@ -1,8 +1,15 @@
 
+import {CellStreamOutput,CellDisplayDataOutput,CellExecuteResultOutput,CellErrorOutput} from "./notebook.model"
+import { SafeHtml} from '@angular/platform-browser';
+
 export interface  Slides {
     source:string[] | string;
     fragments:{text: string[]| string, type:string,language?:string}[];
     children: Slides[];
     type:string;
+    isTrusted:boolean;
+    errorHtml: SafeHtml;
+    streamHtml: SafeHtml;
+    outputs?:(CellStreamOutput | CellDisplayDataOutput | CellExecuteResultOutput | CellErrorOutput)[];
     language?:string;
   }
