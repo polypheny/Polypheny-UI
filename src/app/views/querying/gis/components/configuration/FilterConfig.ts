3cf77bfbbbbbe8ff8d139be398a01c8b4cb3fb36
Rafael Biehler <mail@rafaelbiehler.com>
2024-12-15 16:50:57 +0100

    FilterConfig.ts TsLint

--- src/app/views/querying/gis/components/configuration/FilterConfig.ts
+++ src/app/views/querying/gis/components/configuration/FilterConfig.ts
@@ -1,20 +1,20 @@
-import {MapLayerConfiguration} from "../../models/MapLayerConfiguration.interface";
-import {Polygon} from 'geojson'
-import {FilterComponent} from "./filter/filter.component";
-import {MapLayer} from "../../models/MapLayer.model";
+import {MapLayerConfiguration} from '../../models/MapLayerConfiguration.interface';
+import {Polygon} from 'geojson';
+import {FilterComponent} from './filter/filter.component';
+import {MapLayer} from '../../models/MapLayer.model';
 
 export class FilterConfig implements MapLayerConfiguration {
-    configurationComponentType = FilterComponent
-    filterPolygon : Polygon = null;
-    filterPolygonText = ""
+    configurationComponentType = FilterComponent;
+    filterPolygon: Polygon = null;
+    filterPolygonText = '';
     layer: MapLayer;
 
-    constructor(layer : MapLayer) {
+    constructor(layer: MapLayer) {
         this.layer = layer;
     }
 
     copy(): MapLayerConfiguration {
-        const copy = new FilterConfig(this.layer)
+        const copy = new FilterConfig(this.layer);
         copy.filterPolygon = this.filterPolygon;
         copy.filterPolygonText = this.filterPolygonText;
         return copy;
@@ -22,13 +22,13 @@ export class FilterConfig implements MapLayerConfiguration {
 
     addPolygon(polygon: Polygon) {
         this.filterPolygon = polygon;
-        this.filterPolygonText = "Polygon";
-        console.log("Polygon added", polygon);
+        this.filterPolygonText = 'Polygon';
+        console.log('Polygon added', polygon);
     }
 
     removePolygon() {
         this.filterPolygon = null;
-        this.filterPolygonText = "";
-        console.log("Polygon removed");
+        this.filterPolygonText = '';
+        console.log('Polygon removed');
     }
 }
