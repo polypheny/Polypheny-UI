ded0f088f4928b403beb148c0dccc210ea7644e9
Tobias Weber <tm.weber@stud.unibas.ch>
2025-10-20 13:40:47 +0200

    Prepare for migration to the new build system

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-module.exports = {
-    plugins: [
-        new BundleAnalyzerPlugin({analyzerMode: "json" })
-    ],
-    experiments: {
-        topLevelAwait: true
-    }
-};
